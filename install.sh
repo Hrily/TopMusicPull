90290b810b8eb3a5a015e0426f4ae4b8ce72c213
Salman Shah <sbs.191197@gmail.com>
2016-11-22 14:26:22 +0530

    Locally tested

--- install.sh
+++ install.sh
@@ -2,20 +2,19 @@
 
 #First update the given System
 sudo apt-get update
-
 #Install Python pip to install Python based dependencies
 sudo sudo apt-get install python-pip
 
-#Install the youtube-dl dependency
-sudo pip install youtube-dl
 
 #Refresh to update
 sudo apt-get update
+#Install the youtube-dl dependency
+sudo pip install youtube-dl
 #Install Python Installer
 sudo pip install PyInstaller
 #Install mid3v2
 sudo pip install mutagen
 
 #Move the Binary compiled file to the bin folder
-sudo mv dist/music_crawler /bin
+sudo cp dist/music_crawler /bin
 
